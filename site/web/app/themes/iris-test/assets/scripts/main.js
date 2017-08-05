230f2561c6fa84a8f54dd3a21291867d593b382a
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-08-05 14:52:14 +0200

    Sets main menu styling

--- site/web/app/themes/iris-test/assets/scripts/main.js
+++ site/web/app/themes/iris-test/assets/scripts/main.js
@@ -19,6 +19,12 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
+
+
+
+
+
+
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
