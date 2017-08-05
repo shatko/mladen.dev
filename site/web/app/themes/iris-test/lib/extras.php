a41e902abf164d2798d9570cfe63333eaa9f3bf1
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-08-05 19:44:54 +0200

    Adds Theme Logo to the backend login

--- site/web/app/themes/iris-test/lib/extras.php
+++ site/web/app/themes/iris-test/lib/extras.php
@@ -31,3 +31,28 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Adds Theme Logo to login page
+ */
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/dist/images/NobodysFool.png);
+            background-size: cover;
+            width: 211px;
+            height: 57px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\\my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', __NAMESPACE__ . '\\my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'NobodysFool - Free PSD Website Template';
+}
+add_filter( 'login_headertitle', __NAMESPACE__ . '\\my_login_logo_url_title' );
