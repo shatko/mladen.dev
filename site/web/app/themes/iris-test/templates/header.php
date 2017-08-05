5604994736917220e9eec8257844c76351eae98a
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-08-05 12:25:37 +0200

    Sets basic header structure

--- site/web/app/themes/iris-test/templates/header.php
+++ site/web/app/themes/iris-test/templates/header.php
@@ -1,12 +1,20 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <div class="toggler">
+
+    </div>
+    <div class="nav-primary-wrapper">
+      <nav class="nav-primary">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        endif;
+        ?>
+      </nav>
+    </div>
+    <div class="page-info-wrapper">
+      <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('description'); ?></a>
+    </div>
   </div>
 </header>
