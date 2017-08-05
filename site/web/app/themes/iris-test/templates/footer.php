ab272c033544a0d405a58adfd5988d6287ba1480
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-08-05 13:24:26 +0200

    Sets basic footer structure

--- site/web/app/themes/iris-test/templates/footer.php
+++ site/web/app/themes/iris-test/templates/footer.php
@@ -1,4 +1,21 @@
 <footer class="content-info">
   <div class="container">
+    <div class="footer-segment-wrapper row">
+      <div class="footer-segment-outer col-lg-6">.col-lg-6
+        <div class="row">
+          <div class="footer-about col-md-6">.col-md-6</div>
+          <div class="footer-links col-md-6">.col-md-6</div>
+        </div>
+      </div>
+      <div class="footer-segment-outer footer-links col-lg-6">.col-lg-6
+        <div class="row">
+          <div class="footer-about col-md-6">.col-md-6</div>
+          <div class="footer-links col-md-6">.col-md-6</div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+
+    </div>
   </div>
 </footer>
