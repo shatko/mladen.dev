a9eb968e27711f7954e634b45d0f1121cba8b057
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-08-06 21:19:51 +0200

    Constructs one-image-four-posts part and fixes slider row

--- site/web/app/themes/iris-test/templates/content-page.php
+++ site/web/app/themes/iris-test/templates/content-page.php
@@ -10,13 +10,13 @@
       if( get_row_layout() == 'slider' ):
         ?>
         <div class="slider-wrapper row">
-          <div class="slider-teaser col-lg-6">
+          <div class="slider-teaser col-lg-5">
             <h2><?php the_sub_field('title'); ?></h2>
             <p><?php the_sub_field('description'); ?></p>
             <a href="<?php the_sub_field('read_more_url'); ?>">Read More</a>
           </div>
 
-          <div class="slider col-lg-6">
+          <div class="slider col-lg-7">
             <?php
             $images = get_sub_field('slider_images');
             if( $images ): ?>
@@ -44,12 +44,55 @@
             </div>
           </div>
         </div>
-
         <?php
-      elseif( get_row_layout() == 'call_to _action' ):
-
-        $file = get_sub_field('file');
-
+      elseif( get_row_layout() == 'one-image-four-posts' ):
+        ?>
+        <div class="one-image-four-posts-wrapper">
+          <div class="one-image-four-posts row">
+            <div class="image-left col-lg-6">
+              <img src="<?php echo the_sub_field('insert_image'); ?>" alt="iris" />
+            </div>
+            <div class="posts col-lg-6">
+              <?php $first_post_id = get_sub_field('first_post'); ?>
+              <?php $second_post_id = get_sub_field('second_post'); ?>
+              <?php $third_post_id = get_sub_field('third_post'); ?>
+              <?php $fourth_post_id = get_sub_field('fourth_post'); ?>
+              <div class="row">
+                <div class="post col-md-6">
+                  <div class="image">
+                    <?php echo get_the_post_thumbnail($first_post_id); ?>
+                  </div>
+                  <a href="<?php echo get_the_permalink($first_post_id); ?>"><?php echo get_the_title($first_post_id); ?></a>
+                  <p><?php echo get_the_excerpt($first_post_id); ?></p>
+                </div>
+                <div class="post col-md-6">
+                  <div class="image">
+                    <?php echo get_the_post_thumbnail($second_post_id); ?>
+                  </div>
+                  <a href="<?php echo get_the_permalink($second_post_id); ?>"><?php echo get_the_title($second_post_id); ?></a>
+                  <p><?php echo get_the_excerpt($second_post_id); ?></p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="post col-md-6">
+                  <div class="image">
+                    <?php echo get_the_post_thumbnail($third_post_id); ?>
+                  </div>
+                  <a href="<?php echo get_the_permalink($third_post_id); ?>"><?php echo get_the_title($third_post_id); ?></a>
+                  <p><?php echo get_the_excerpt($third_post_id); ?></p>
+                </div>
+                <div class="post col-md-6">
+                  <div class="image">
+                    <?php echo get_the_post_thumbnail($fourth_post_id); ?>
+                  </div>
+                  <a href="<?php echo get_the_permalink($fourth_post_id); ?>"><?php echo get_the_title($fourth_post_id); ?></a>
+                  <p><?php echo get_the_excerpt($fourth_post_id); ?></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php
       endif;
     endwhile;
   else :
