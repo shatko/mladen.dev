d142c2650479779fdf64d92f77aa4e3aced59f65
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-08-06 10:25:48 +0200

    Sets About us and Quick links segments in the footer

--- site/web/app/themes/iris-test/templates/footer.php
+++ site/web/app/themes/iris-test/templates/footer.php
@@ -1,21 +1,45 @@
 <footer class="content-info">
   <div class="container">
     <div class="footer-segment-wrapper row">
-      <div class="footer-segment-outer col-lg-6">.col-lg-6
+      <div class="col-lg-6">
         <div class="row">
-          <div class="footer-about col-md-6">.col-md-6</div>
-          <div class="footer-links col-md-6">.col-md-6</div>
+          <div class="footer-segment footer-about col-md-6">
+            <p class="footer-title">About us</p>
+            <img src="http://iris.shatko.party/app/uploads/2017/08/215x90px.jpg" alt="iris">
+            <p class="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non diam erat. In fringilla massa ut nisi ullamcorper pellentesque. Quisque non luctus sem. Nullam non magna vel nisi posuere bibendum vitae sed dui.</p>
+            <a class="read-more" href="#">Read More</a>
+          </div>
+          <div class="footer-segment footer-links col-md-6">
+            <p class="footer-title">Quick Links</p>
+            <ul>
+              <li><a href="#">Lorem ipsum dolor sit</a></li>
+              <li><a href="#">Amet consectetur</a></li>
+              <li><a href="#">Praesent vel sem id</a></li>
+              <li><a href="#">Curabitur hendrerit est</a></li>
+              <li><a href="#">Aliquam eget erat nec sapien</a></li>
+              <li><a href="#">Cras id augue nunc</a></li>
+              <li><a href="#">In nec justo non</a></li>
+              <li><a href="#">Vivamus mollis enim ut</a></li>
+              <li><a href="#">Curabitur hendrerit est</a></li>
+              <li><a href="#">Sed a nulla urna</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div class="footer-segment-outer footer-links col-lg-6">.col-lg-6
+      <div class="col-lg-6">
         <div class="row">
-          <div class="footer-about col-md-6">.col-md-6</div>
-          <div class="footer-links col-md-6">.col-md-6</div>
+          <div class="footer-segment footer-about col-md-6">
+            <p class="footer-title">Latest Blog posts</p>
+          </div>
+          <div class="footer-segment footer-links col-md-6">
+            <p class="footer-title">Contact Us</p>
+          </div>
         </div>
       </div>
     </div>
-    <div class="footer-bottom">
-
+    <div class="footer-bottom row">
+      <p class="col-md-6">Copyright © 2013 Domain Name - All Rights Reserved</p>
+      <p class="col-md-6">Template by OS Templates</p>
     </div>
   </div>
 </footer>
