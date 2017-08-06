aefbe0bbfb98c047733c40204aedbbfdfe94a914
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-08-06 08:48:28 +0200

    Constructs call to action part

--- site/web/app/themes/iris-test/templates/content-page.php
+++ site/web/app/themes/iris-test/templates/content-page.php
@@ -10,13 +10,13 @@
       if( get_row_layout() == 'slider' ):
         ?>
         <div class="slider-wrapper row">
-          <div class="slider-teaser col-lg-6 col-lg-pull-6">
+          <div class="slider-teaser col-lg-6">
             <h2><?php the_sub_field('title'); ?></h2>
             <p><?php the_sub_field('description'); ?></p>
             <a href="<?php the_sub_field('read_more_url'); ?>">Read More</a>
           </div>
 
-          <div class="slider col-lg-6 col-lg-push-6">
+          <div class="slider col-lg-6">
             <?php
             $images = get_sub_field('slider_images');
             if( $images ): ?>
@@ -31,9 +31,24 @@
           </div>
         </div>
         <?php
-      elseif( get_row_layout() == 'download' ):
+      elseif( get_row_layout() == 'call_to _action' ):
+        ?>
+        <div class="call-to-action-wrapper">
+          <div class="call-to-action row" style="<?php the_sub_field('border_position'); ?>:10px solid #0099ff">
+            <div class="text-wrapper col-lg-8">
+              <h2><?php the_sub_field('headline'); ?></h2>
+              <h4><?php the_sub_field('minititle'); ?></h4>
+            </div>
+            <div class="button-wrapper col-lg-4">
+              <a href="<?php the_sub_field('button_url'); ?>"><?php the_sub_field('button_text'); ?></a>
+            </div>
+          </div>
+        </div>
 
-      	$file = get_sub_field('file');
+        <?php
+      elseif( get_row_layout() == 'call_to _action' ):
+
+        $file = get_sub_field('file');
 
       endif;
     endwhile;
@@ -41,7 +56,3 @@
   endif;
 
   ?>
-  <div class="row">
-    <div style="background:yellow" class="col-md-9 col-md-push-3">.col-md-9 .col-md-push-3</div>
-    <div style="background:green" class="col-md-3 col-md-pull-9">.col-md-3 .col-md-pull-9</div>
-  </div>
